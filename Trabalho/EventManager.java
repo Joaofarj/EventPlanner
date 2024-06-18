004e9b41ae67bdab394a7aa86a39a81f9cad59ea
Joaofarj <87398892+Joaofarj@users.noreply.github.com>
2024-06-17 22:48:08 -0300

    Update EventManager.java

--- Trabalho/EventManager.java
+++ Trabalho/EventManager.java
@@ -4,19 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The EventManager class is responsible for managing a list of events.
+ * It includes functionalities to add, edit, delete, search, and retrieve events.
+ * It also handles loading and saving events to a file.
+ */
 public class EventManager {
     private List<Event> events;
 
+    /**
+     * Constructs an EventManager instance and loads events from the specified file.
+     *
+     * @param filename the name of the file to load events from
+     */
     public EventManager(String filename) {
         events = loadEvents();
     }
 
-    // Method to add an event
+    /**
+     * Adds an event to the list.
+     *
+     * @param event the event to add
+     */
     public void addEvent(Event event) {
         events.add(event);
     }
 
-    // Method to edit an event
+    /**
+     * Edits an existing event.
+     *
+     * @param oldEvent the event to be replaced
+     * @param newEvent the new event to replace the old event
+     */
     public void editEvent(Event oldEvent, Event newEvent) {
         int index = events.indexOf(oldEvent);
         if (index >= 0) {
@@ -24,26 +43,44 @@ public class EventManager {
         }
     }
 
-    // Method to delete an event
+    /**
+     * Deletes an event from the list.
+     *
+     * @param event the event to delete
+     */
     public void deleteEvent(Event event) {
         events.remove(event);
     }
 
-    // Method to search for events by keyword
+    /**
+     * Searches for events by a keyword. The search is performed on event titles and descriptions.
+     *
+     * @param keyword the keyword to search for
+     * @return a list of events that match the keyword
+     */
     public List<Event> searchEvents(String keyword) {
         return events.stream()
                 .filter(event -> event.getTitle().contains(keyword) || event.getDescription().contains(keyword))
                 .collect(Collectors.toList());
     }
 
-    // Method to get events on a specific date
+    /**
+     * Retrieves a list of events occurring on a specific date.
+     *
+     * @param date the date to filter events by
+     * @return a list of events occurring on the specified date
+     */
     public List<Event> getEventsByDate(LocalDate date) {
         return events.stream()
                 .filter(event -> event.getDate().equals(date))
                 .collect(Collectors.toList());
     }
 
-    // Method to load events from a file
+    /**
+     * Loads events from the "events.dat" file.
+     *
+     * @return a list of loaded events
+     */
     @SuppressWarnings("unchecked")
     public List<Event> loadEvents() {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("events.dat"))) {
@@ -53,7 +90,9 @@ public class EventManager {
         }
     }
 
-    // Method to save events to a file
+    /**
+     * Saves the current list of events to the "events.dat" file.
+     */
     public void saveEvents() {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("events.dat"))) {
             oos.writeObject(events);
@@ -61,9 +100,12 @@ public class EventManager {
             e.printStackTrace();
         }
     }
-    
 
-    // Method to list all events (for debugging purposes)
+    /**
+     * Retrieves all events for debugging purposes.
+     *
+     * @return a list of all events
+     */
     public List<Event> getAllEvents() {
         return new ArrayList<>(events);
     }
